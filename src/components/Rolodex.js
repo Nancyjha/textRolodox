2c1f2f1d5977f6729071ba02406c84be72d493a2
Nancyjha <nancyvats00@gmail.com>
2022-08-03 21:05:36 +0530

    axios

--- src/components/Rolodex.js
+++ src/components/Rolodex.js
@@ -1,17 +1,30 @@
 import React,{useState} from 'react'; 
+const axios = require("axios");
 
 function Rolodex(props){
+
+    const spellCheck=()=>{
+        const url="https://jspell-checker.p.rapidapi.com/check"
+        const headers ={
+            'content-type': 'application/json',
+            'X-RapidAPI-Host': 'jspell-checker.p.rapidapi.com',
+            'X-RapidAPI-Key': '2ee909b39dmsh0bc45c14e13b7a6p14bbbbjsndfbd08c39b9c'
+        }
+        const data= {"language":"enUS","fieldvalues":text,"config":{"forceUpperCase":false,"ignoreIrregularCaps":false,"ignoreFirstCaps":true,"ignoreNumbers":true,"ignoreUpper":false,"ignoreDouble":false,"ignoreWordsWithNumbers":true}}
+
+axios.post(url,JSON.stringify(data) ,{
+    headers:headers
+}).then(response => {response.data.elements[0].errors.forEach(ele=>{alert(`word:${ele.word}\nposition:${ele.position}\nsuggestions:${ele.suggestions.map(suggestion=>{return `\n${suggestion}`})}\n`)})}).catch(function (error) {
+    console.error(error);
+});
+    }
     
     const handleUpCase=()=>{
-        console.log('eheh');
         let newText=text.toLocaleUpperCase();
         setText(newText);
-        
     }
     const handleOnChange=(event)=>{
-        console.log('hehe')
          setText(event.target.value);
-        
     }
     const handleLoCase=()=>{
         let newText =text.toLocaleLowerCase();
@@ -24,8 +37,8 @@ function Rolodex(props){
 <div className="mb-3" >
   <textarea onChange={handleOnChange} style={{backgroundColor:props.mode==="light" ? "white" : "grey", color:props.mode==="dark"?"white": "black"}} className="form-control" value={text} id=" mybox" rows="8"></textarea>
   <button type="button" className="btn btn-dark my-3 " onClick={handleUpCase}> convert to uppercase</button>
-
   <button type="button" className="btn btn-dark my-3 mx-3 " onClick={handleLoCase}> convert to uppercase</button>
+  <button type="button" className="btn btn-dark my-3 mx-3 " onClick={spellCheck}> Check for spell</button>
 
 </div>
 <div className="container my-3">
