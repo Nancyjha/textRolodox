9685bb3d6602f59d87251d04fc45ccce5743a938
Nancyjha <nancyvats00@gmail.com>
2022-08-03 21:22:01 +0530

    fixes

--- src/components/Rolodex.js
+++ src/components/Rolodex.js
@@ -13,8 +13,17 @@ function Rolodex(props){
         const data= {"language":"enUS","fieldvalues":text,"config":{"forceUpperCase":false,"ignoreIrregularCaps":false,"ignoreFirstCaps":true,"ignoreNumbers":true,"ignoreUpper":false,"ignoreDouble":false,"ignoreWordsWithNumbers":true}}
 
 axios.post(url,JSON.stringify(data) ,{
+
     headers:headers
-}).then(response => {response.data.elements[0].errors.forEach(ele=>{alert(`word:${ele.word}\nposition:${ele.position}\nsuggestions:${ele.suggestions.map(suggestion=>{return `\n${suggestion}`})}\n`)})}).catch(function (error) {
+}).then(response => {
+    console.log(text);
+    if(response.data.hasOwnProperty("elements")){
+
+        response.data.elements[0].errors.forEach(ele=>{alert(`word:${ele.word}\nposition:${ele.position}\nsuggestions:${ele.suggestions.map(suggestion=>{return `\n${suggestion}`})}\n`)})
+    }else{
+        alert('No errors found...')
+    }
+}).catch(function (error) {
     console.error(error);
 });
     }
@@ -37,13 +46,13 @@ axios.post(url,JSON.stringify(data) ,{
 <div className="mb-3" >
   <textarea onChange={handleOnChange} style={{backgroundColor:props.mode==="light" ? "white" : "grey", color:props.mode==="dark"?"white": "black"}} className="form-control" value={text} id=" mybox" rows="8"></textarea>
   <button type="button" className="btn btn-dark my-3 " onClick={handleUpCase}> convert to uppercase</button>
-  <button type="button" className="btn btn-dark my-3 mx-3 " onClick={handleLoCase}> convert to uppercase</button>
+  <button type="button" className="btn btn-dark my-3 mx-3 " onClick={handleLoCase}> convert to lowercase</button>
   <button type="button" className="btn btn-dark my-3 mx-3 " onClick={spellCheck}> Check for spell</button>
 
 </div>
 <div className="container my-3">
     <h1>your text summary</h1>
-    <p>number of words and characters in above paragraph is {text.split('').length} and {text.length} respectively</p>
+    <p>number of characters in above paragraph is {text.length} </p>
     <p>you can read it in {0.008*text.split('').length} minutes</p>
    
     
